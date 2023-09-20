3dda2332df70ac084762c242cc33878e7b4e5c42
balle0056 <baan20ac@student.cbs.dk>
2023-09-20 13:04:13 +0200

    Update FetchListComponent.js

--- components/FetchListComponent.js
+++ components/FetchListComponent.js
@@ -1,8 +1,12 @@
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+
 const FetchListComponent = (props) => { 
     return (
         <Text>Dette er vores Fetchlist</Text>
     )
 }
+
 const styles = StyleSheet.create({
     container: {
       backgroundColor: '#fff',
