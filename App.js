cc26d8f0beea7523de0af51b273a3cf3a0c88064
victobach <visv21ab@student.cbs.dk>
2023-09-20 15:42:44 +0200

    Added List to Venues

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-
+import { Text, View, StyleSheet } from "react-native"; // Import StyleSheet
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import StackNavigatorVenues from "./components/StackNavigatorVenues";
@@ -9,24 +8,35 @@ import Premium from "./components/Premium";
 import ClubOwner from "./components/ClubOwner";
 import StackNavigatorProfile from "./components/StackNavigatorProfile";
 import Venues from "./components/stackComponentsVenues/Venues";
-
+import ArrayListComponent from "./components/venueListComponents/ArrayListComponent";
+import FetchListComponent from "./components/venueListComponents/FetchListComponent";
+import FlatListComponent from "./components/venueListComponents/FlatListComponent";
+import { VenueList } from "./BarLists";
+import { Area } from "./BarLists";
+import { GetUserURL } from "./BarLists";
 
 const Tab = createBottomTabNavigator();
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FF0000",
+  },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Venues Map" component={StackNavigatorVenues} />
-
-        <Tab.Screen name="Profile" component={StackNavigatorProfile} />
-
-        <Tab.Screen name="Premium" component={Premium} />
-
-            <Tab.Screen name="ClubOwner" component={ClubOwner} />
-
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Venue list" component={ArrayListComponent} />
+          <Tab.Screen name="Venues Map" component={StackNavigatorVenues} />
+          <Tab.Screen name="Profile" component={StackNavigatorProfile} />
+          <Tab.Screen name="Premium" component={Premium} />
+          <Tab.Screen name="ClubOwner" component={ClubOwner} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
---
+++ BarLists.js
@@ -0,0 +1,22 @@
+export const VenueList = [
+  "BadaBing",
+  "A-Bar",
+  "Zoo",
+  "Aloha Bar",
+  "Wallstreet pub",
+  "Angels Club",
+  "Øl",
+  "Gorilla",
+  "Old Irish Pub",
+  "Café Nexus",
+  "Bistro Central",
+  "Café Det Elektriske Hjørne",
+  "Levi's",
+  "Café Victor",
+];
+
+export const Area = ["København K", "København NV", "Frederiksberg"];
+
+export const GetUserURL = "https://randomuser.me/api?results=";
+
+console.log(VenueList);
