0ee212d752a9d79930476b6e0876cfb5c11b6895
Chrilzzz <91545589+Chrilzzz@users.noreply.github.com>
2023-09-24 20:58:34 +0200

    Update MyBug.js Imported the my bug.js view.

--- components/StackComponentsSettings/MyBug.js
+++ components/StackComponentsSettings/MyBug.js
@@ -1,1 +1,46 @@
 
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function MyBugScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.goBackButton}
+        >
+          <Ionicons name="ios-arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Report Bug</Text>
+      </View>
+      <Text>This is the Report Bug screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    elevation: 2, // Add elevation for a modern card-like effect
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+  },
+  goBackButton: {
+    marginRight: 10,
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
