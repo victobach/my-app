8d424556900ee8e822eb161ca5b34cb3bcddd0a6
benjaminblom12 <90244878+benjaminblom12@users.noreply.github.com>
2023-09-24 20:17:50 +0200

    Create UserLogin.js

---
+++ components/UserLogin.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Image } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+
+const navController = (navigation, route) => {
+  navigation.navigate(route);
+};
+
+export default function LoginScreen({ navigation }) {
+  const [clubName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEwykSQwC2GMgWNMdaVWlfvkKjTfc-uXY",
+    authDomain: "innovationogtekt.firebaseapp.com",
+    databaseURL:
+      "https://innovationogtekt-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "innovationogtekt",
+    storageBucket: "innovationogtekt.appspot.com",
+    messagingSenderId: "378823600165",
+    appId: "1:378823600165:web:3c7edb88d421c4aed177cc",
+    measurementId: "G-HRRR03JBJL",
+  };
+
+  const handleLogin = async () => {
+    setIsLoading(true);
+    setIsLoading(false);
+  };
+
+  initializeApp(firebaseConfig);
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} />
+      <TextInput
+        placeholder="User Login"
+        value={clubName}
+        onChangeText={(text) => setUserName(text)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} disabled={isLoading} />
+      <Button
+        title="Forgot Password?"
+        onPress={() => navController(navigation, "ForgotPassword")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+  },
+  footerText: {
+    marginTop: 20,
+    color: "blue",
+  },
+});
